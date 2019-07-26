a67afc02b48e2dfe235931a3b5e0751c89891246
Sam Cook <WhenPotatoesFry@gmail.com>
2019-07-26 10:18:20 -0700

    update ruby, install and research heroku, install required gems.

---
+++ app.rb
@@ -0,0 +1,6 @@
+require 'pry'
+require 'words.rb'
+
+get '/' do
+  erb(:index)
+end
--- lib/words.rb
+++ lib/words.rb
@@ -1,1 +1,10 @@
 require 'pry'
+
+class Word
+  @@words = {}
+  @@total_words = 0
+  def initialize(attributes)
+    @word = attributes.fetch(:word)
+    @word_id = @@total_words += 1
+  end
+end
